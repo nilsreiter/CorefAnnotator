1a4fd97e92979a135b3886dbbc79517d5b77047a
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-02-02 11:41:23 +0100

    better colors

--- src/main/java/de/unistuttgart/ims/coref/annotator/ColorMap.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/ColorMap.java
@@ -8,8 +8,29 @@ public class ColorMap {
 
 	int nextColor = 0;
 
+	float lowest = 0.2f;
+	float highest = 0.8f;
+
+	float r = lowest;
+	float g = 0.5f;
+	float b = highest;
+	float delta = 0.1f;
+
 	public Color getNextColor() {
-		return colors[nextColor++ % colors.length];
+		Color c = new Color(r, g, b);
+
+		r += delta;
+		if (r > highest)
+			r = lowest;
+		g += delta;
+		if (g > highest)
+			g = lowest;
+		b -= delta;
+		if (b < lowest)
+			b = highest;
+
+		return c;
+		// return colors[nextColor++ % colors.length];
 	}
 
 }
