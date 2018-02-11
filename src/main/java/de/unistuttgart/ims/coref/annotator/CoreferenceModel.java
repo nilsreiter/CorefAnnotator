9f10153b7c067bf022b5bf0865cdcbdeaa2bc534
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-02-11 07:55:36 +0100

    sorted all members

--- src/main/java/de/unistuttgart/ims/coref/annotator/CoreferenceModel.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/CoreferenceModel.java
@@ -30,28 +30,28 @@ import de.unistuttgart.ims.coref.annotator.api.Mention;
 import de.unistuttgart.ims.uimautil.AnnotationUtil;
 
 public class CoreferenceModel extends DefaultTreeModel implements TreeSelectionListener {
-	JCas jcas;
 	private static final long serialVersionUID = 1L;
-	Map<FeatureStructure, EntityTreeNode> entityMap = new HashMap<FeatureStructure, EntityTreeNode>();
-	Map<FeatureStructure, CATreeNode> mentionMap = new HashMap<FeatureStructure, CATreeNode>();
-	Map<Character, Entity> keyMap = new HashMap<Character, Entity>();
-	ColorProvider colorMap = new ColorProvider();
 	RangedHashSetValuedHashMap<Annotation> characterPosition2AnnotationMap = new RangedHashSetValuedHashMap<Annotation>();
-	List<CoreferenceModelListener> crModelListeners = new LinkedList<CoreferenceModelListener>();
-
+	ColorProvider colorMap = new ColorProvider();
 	HashSetValuedHashMap<FeatureStructure, Comment> comments = new HashSetValuedHashMap<FeatureStructure, Comment>();
+	List<CoreferenceModelListener> crModelListeners = new LinkedList<CoreferenceModelListener>();
+	Map<FeatureStructure, EntityTreeNode> entityMap = new HashMap<FeatureStructure, EntityTreeNode>();
+	EntitySortOrder entitySortOrder = EntitySortOrder.Alphabet;
+	CATreeNode groupRootNode;
+
+	JCas jcas;
+	boolean keepEmptyEntities = true;
+
 	int key = 0;
 
 	char[] keyCodes = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	Map<Character, Entity> keyMap = new HashMap<Character, Entity>();
 
-	CATreeNode rootNode;
-	CATreeNode groupRootNode;
-
-	EntitySortOrder entitySortOrder = EntitySortOrder.Alphabet;
-
-	boolean keepEmptyEntities = true;
+	Map<FeatureStructure, CATreeNode> mentionMap = new HashMap<FeatureStructure, CATreeNode>();
 
 	Preferences preferences;
+
+	CATreeNode rootNode;
 
 	public CoreferenceModel(JCas jcas, Preferences preferences) {
 		super(new CATreeNode(null, Annotator.getString("tree.root")));
@@ -64,71 +64,38 @@ public class CoreferenceModel extends DefaultTreeModel implements TreeSelectionL
 
 	}
 
-	public void toggleFlagMention(Mention m, String flag) {
-		if (Util.contains(m.getFlags(), flag)) {
-			m.setFlags(Util.removeFrom(jcas, m.getFlags(), flag));
-		} else
-			m.setFlags(Util.addTo(jcas, m.getFlags(), flag));
-		nodeChanged(mentionMap.get(m));
+	public boolean addCoreferenceModelListener(CoreferenceModelListener e) {
+		return crModelListeners.add(e);
+	}
+
+	public void addDiscontinuousToMention(Mention m, DetachedMentionPart dmp) {
+		m.setDiscontinuous(dmp);
+		insertNodeInto(mentionMap.get(dmp), mentionMap.get(m), 0);
+	}
+
+	public void addDiscontinuousToMention(Mention m, int begin, int end) {
+		DetachedMentionPart d = AnnotationFactory.createAnnotation(jcas, begin, end, DetachedMentionPart.class);
+		d.setMention(m);
+		m.setDiscontinuous(d);
+		characterPosition2AnnotationMap.add(d);
+		CATreeNode node = new CATreeNode(d, d.getCoveredText());
+		mentionMap.put(d, node);
+		insertNodeInto(node, mentionMap.get(m), 0);
 		fireMentionChangedEvent(m);
-		fireMentionSelectedEvent(m);
 	}
 
-	public void toggleFlagEntity(Entity m, String flag) {
-		if (Util.contains(m.getFlags(), flag)) {
-			m.setFlags(Util.removeFrom(jcas, m.getFlags(), flag));
-		} else
-			m.setFlags(Util.addTo(jcas, m.getFlags(), flag));
-		nodeChanged(mentionMap.get(m));
-		// fireMentionChangedEvent(m);
-		// fireMentionSelectedEvent(m);
-	}
-
-	public void updateMention(Mention m, Entity newEntity) {
-		// remove mention from old entity
-		removeNodeFromParent(mentionMap.get(m));
-		mentionMap.remove(m);
-
-		// attach it to the new entity
-		connect(newEntity, m);
-
-		// fire event
-		this.fireMentionChangedEvent(m);
-	}
-
-	public void removeEntity(Entity e) {
-		removeNodeFromParent(entityMap.get(e));
-		e.removeFromIndexes();
-		entityMap.remove(e);
-		String k = e.getKey();
-		if (k != null)
-			keyMap.remove(k.charAt(0));
-	}
-
-	public void removeEntityGroup(EntityGroup eg) {
-		EntityTreeNode etn = entityMap.get(eg);
-		for (int i = 0; i < etn.getChildCount(); i++) {
-			FeatureStructure fs = etn.getChildAt(i).getFeatureStructure();
-			if (fs instanceof Mention)
-				removeMention((Mention) fs);
+	public EntityTreeNode addExistingEntity(Entity e) {
+		EntityTreeNode tn = new EntityTreeNode(e, "");
+		// if (e instanceof EntityGroup) {
+		// insertNodeInto(tn, groupRootNode, 0);
+		// } else
+		insertNodeInto(tn, rootNode, 0);
+		entityMap.put(e, tn);
+		if (e.getKey() != null) {
+			tn.setKeyCode(e.getKey().charAt(0));
+			keyMap.put(e.getKey().charAt(0), e);
 		}
-		removeNodeFromParent(etn);
-		eg.removeFromIndexes();
-
-	}
-
-	public void removeEntityFromGroup(EntityGroup eg, EntityTreeNode e) {
-		removeNodeFromParent(e);
-		FSArray oldArray = eg.getMembers();
-		FSArray arr = new FSArray(jcas, eg.getMembers().size() - 1);
-		for (int i = 0; i < oldArray.size() - 1; i++) {
-			if (eg.getMembers(i) == e.getFeatureStructure()) {
-				i--;
-			} else {
-				arr.set(i, eg.getMembers(i));
-			}
-		}
-		eg.setMembers(arr);
+		return tn;
 	}
 
 	public void addNewEntityMention(int begin, int end) {
@@ -162,18 +129,31 @@ public class CoreferenceModel extends DefaultTreeModel implements TreeSelectionL
 		addNewMention(e, begin, end);
 	}
 
-	public EntityTreeNode addExistingEntity(Entity e) {
-		EntityTreeNode tn = new EntityTreeNode(e, "");
-		// if (e instanceof EntityGroup) {
-		// insertNodeInto(tn, groupRootNode, 0);
-		// } else
-		insertNodeInto(tn, rootNode, 0);
-		entityMap.put(e, tn);
-		if (e.getKey() != null) {
-			tn.setKeyCode(e.getKey().charAt(0));
-			keyMap.put(e.getKey().charAt(0), e);
+	public void addNewMention(Entity e, int begin, int end) {
+		Mention m = new Mention(jcas);
+		m.setBegin(begin);
+		m.setEnd(end);
+		if (preferences.getBoolean(Constants.CFG_TRIM_WHITESPACE, true))
+			m = AnnotationUtil.trim(m);
+		m.addToIndexes();
+		connect(e, m);
+		fireMentionAddedEvent(m);
+	}
+
+	public void addToGroup(EntityGroup eg, Entity e) {
+		// UIMA stuff
+		FSArray oldArr = eg.getMembers();
+		FSArray arr = new FSArray(jcas, eg.getMembers().size() + 1);
+		int i = 0;
+		for (; i < eg.getMembers().size(); i++) {
+			arr.set(i, eg.getMembers(i));
 		}
-		return tn;
+		arr.set(i, e);
+		eg.setMembers(arr);
+		oldArr.removeFromIndexes();
+
+		// tree stuff
+		insertNodeInto(new EntityTreeNode(e), entityMap.get(eg), 0);
 	}
 
 	protected void connect(Entity e, Mention m) {
@@ -202,31 +182,26 @@ public class CoreferenceModel extends DefaultTreeModel implements TreeSelectionL
 		resort();
 	}
 
-	public void addNewMention(Entity e, int begin, int end) {
-		Mention m = new Mention(jcas);
-		m.setBegin(begin);
-		m.setEnd(end);
-		if (preferences.getBoolean(Constants.CFG_TRIM_WHITESPACE, true))
-			m = AnnotationUtil.trim(m);
-		m.addToIndexes();
-		connect(e, m);
-		fireMentionAddedEvent(m);
+	public void fireMentionAddedEvent(Mention m) {
+		this.characterPosition2AnnotationMap.add(m);
+		for (CoreferenceModelListener l : crModelListeners)
+			l.mentionAdded(m);
 	}
 
-	public void addToGroup(EntityGroup eg, Entity e) {
-		// UIMA stuff
-		FSArray oldArr = eg.getMembers();
-		FSArray arr = new FSArray(jcas, eg.getMembers().size() + 1);
-		int i = 0;
-		for (; i < eg.getMembers().size(); i++) {
-			arr.set(i, eg.getMembers(i));
-		}
-		arr.set(i, e);
-		eg.setMembers(arr);
-		oldArr.removeFromIndexes();
+	public void fireMentionChangedEvent(Mention m) {
+		for (CoreferenceModelListener l : crModelListeners)
+			l.mentionChanged(m);
+	}
 
-		// tree stuff
-		insertNodeInto(new EntityTreeNode(e), entityMap.get(eg), 0);
+	public void fireMentionRemovedEvent(Mention m) {
+		this.characterPosition2AnnotationMap.remove(m);
+		for (CoreferenceModelListener l : crModelListeners)
+			l.mentionRemoved(m);
+	}
+
+	public void fireMentionSelectedEvent(Mention m) {
+		for (CoreferenceModelListener l : crModelListeners)
+			l.annotationSelected(m);
 	}
 
 	public void formGroup(Entity e1, Entity e2) {
@@ -253,21 +228,33 @@ public class CoreferenceModel extends DefaultTreeModel implements TreeSelectionL
 		return jcas;
 	}
 
+	public Collection<Annotation> getMentions(int position) {
+		return this.characterPosition2AnnotationMap.get(position);
+	}
+
+	public CATreeNode getNode(Mention m) {
+		return mentionMap.get(m);
+	}
+
 	public boolean isKeyUsed(int i) {
 		return keyMap.containsKey(i);
 	}
 
-	public DetachedMentionPart removeDiscontinuousMentionPart(Mention m) {
-		DetachedMentionPart dmp = m.getDiscontinuous();
-		m.setDiscontinuous(null);
-
-		removeNodeFromParent(mentionMap.get(dmp));
-		return dmp;
+	public void reassignKey(char keyCode, Entity e) {
+		Entity old = keyMap.get(keyCode);
+		if (old != null) {
+			entityMap.get(old).setKeyCode(Character.MIN_VALUE);
+			old.setKey(null);
+			nodeChanged(entityMap.get(old));
+		}
+		keyMap.put(keyCode, e);
+		entityMap.get(e).setKeyCode(keyCode);
+		e.setKey(String.valueOf(keyCode));
+		nodeChanged(entityMap.get(e));
 	}
 
-	public void addDiscontinuousToMention(Mention m, DetachedMentionPart dmp) {
-		m.setDiscontinuous(dmp);
-		insertNodeInto(mentionMap.get(dmp), mentionMap.get(m), 0);
+	public boolean removeCoreferenceModelListener(Object o) {
+		return crModelListeners.remove(o);
 	}
 
 	public void removeDetachedMentionPart(Mention m, DetachedMentionPart dmp) {
@@ -278,19 +265,61 @@ public class CoreferenceModel extends DefaultTreeModel implements TreeSelectionL
 		fireMentionChangedEvent(m);
 	}
 
-	public void addDiscontinuousToMention(Mention m, int begin, int end) {
-		DetachedMentionPart d = AnnotationFactory.createAnnotation(jcas, begin, end, DetachedMentionPart.class);
-		d.setMention(m);
-		m.setDiscontinuous(d);
-		characterPosition2AnnotationMap.add(d);
-		CATreeNode node = new CATreeNode(d, d.getCoveredText());
-		mentionMap.put(d, node);
-		insertNodeInto(node, mentionMap.get(m), 0);
-		fireMentionChangedEvent(m);
+	public DetachedMentionPart removeDiscontinuousMentionPart(Mention m) {
+		DetachedMentionPart dmp = m.getDiscontinuous();
+		m.setDiscontinuous(null);
+
+		removeNodeFromParent(mentionMap.get(dmp));
+		return dmp;
 	}
 
-	public Collection<Annotation> getMentions(int position) {
-		return this.characterPosition2AnnotationMap.get(position);
+	public void removeEntity(Entity e) {
+		removeNodeFromParent(entityMap.get(e));
+		e.removeFromIndexes();
+		entityMap.remove(e);
+		String k = e.getKey();
+		if (k != null)
+			keyMap.remove(k.charAt(0));
+	}
+
+	public void removeEntityFromGroup(EntityGroup eg, EntityTreeNode e) {
+		removeNodeFromParent(e);
+		FSArray oldArray = eg.getMembers();
+		FSArray arr = new FSArray(jcas, eg.getMembers().size() - 1);
+		for (int i = 0; i < oldArray.size() - 1; i++) {
+			if (eg.getMembers(i) == e.getFeatureStructure()) {
+				i--;
+			} else {
+				arr.set(i, eg.getMembers(i));
+			}
+		}
+		eg.setMembers(arr);
+	}
+
+	public void removeEntityGroup(EntityGroup eg) {
+		EntityTreeNode etn = entityMap.get(eg);
+		for (int i = 0; i < etn.getChildCount(); i++) {
+			FeatureStructure fs = etn.getChildAt(i).getFeatureStructure();
+			if (fs instanceof Mention)
+				removeMention((Mention) fs);
+		}
+		removeNodeFromParent(etn);
+		eg.removeFromIndexes();
+
+	}
+
+	public void removeMention(Mention m) {
+		CATreeNode parent = (CATreeNode) mentionMap.get(m).getParent();
+		int index = parent.getIndex(mentionMap.get(m));
+		parent.remove(mentionMap.get(m));
+		nodesWereRemoved(parent, new int[] { index }, new Object[] { mentionMap.get(m) });
+		fireMentionRemovedEvent(m);
+		mentionMap.remove(m);
+		m.removeFromIndexes();
+	}
+
+	public void resort() {
+		resort(entitySortOrder.getComparator());
 	}
 
 	public void resort(Comparator<EntityTreeNode> comparator) {
@@ -315,21 +344,24 @@ public class CoreferenceModel extends DefaultTreeModel implements TreeSelectionL
 		nodeStructureChanged(rootNode);
 	}
 
-	public void resort() {
-		resort(entitySortOrder.getComparator());
+	public void toggleFlagEntity(Entity m, String flag) {
+		if (Util.contains(m.getFlags(), flag)) {
+			m.setFlags(Util.removeFrom(jcas, m.getFlags(), flag));
+		} else
+			m.setFlags(Util.addTo(jcas, m.getFlags(), flag));
+		nodeChanged(mentionMap.get(m));
+		// fireMentionChangedEvent(m);
+		// fireMentionSelectedEvent(m);
 	}
 
-	public void reassignKey(char keyCode, Entity e) {
-		Entity old = keyMap.get(keyCode);
-		if (old != null) {
-			entityMap.get(old).setKeyCode(Character.MIN_VALUE);
-			old.setKey(null);
-			nodeChanged(entityMap.get(old));
-		}
-		keyMap.put(keyCode, e);
-		entityMap.get(e).setKeyCode(keyCode);
-		e.setKey(String.valueOf(keyCode));
-		nodeChanged(entityMap.get(e));
+	public void toggleFlagMention(Mention m, String flag) {
+		if (Util.contains(m.getFlags(), flag)) {
+			m.setFlags(Util.removeFrom(jcas, m.getFlags(), flag));
+		} else
+			m.setFlags(Util.addTo(jcas, m.getFlags(), flag));
+		nodeChanged(mentionMap.get(m));
+		fireMentionChangedEvent(m);
+		fireMentionSelectedEvent(m);
 	}
 
 	public void updateColor(Entity entity, Color newColor) {
@@ -343,49 +375,21 @@ public class CoreferenceModel extends DefaultTreeModel implements TreeSelectionL
 		}
 	}
 
-	public void fireMentionChangedEvent(Mention m) {
-		for (CoreferenceModelListener l : crModelListeners)
-			l.mentionChanged(m);
-	}
+	public void updateMention(Mention m, Entity newEntity) {
+		// remove mention from old entity
+		removeNodeFromParent(mentionMap.get(m));
+		mentionMap.remove(m);
 
-	public void fireMentionAddedEvent(Mention m) {
-		this.characterPosition2AnnotationMap.add(m);
-		for (CoreferenceModelListener l : crModelListeners)
-			l.mentionAdded(m);
-	}
+		// attach it to the new entity
+		connect(newEntity, m);
 
-	public void fireMentionRemovedEvent(Mention m) {
-		this.characterPosition2AnnotationMap.remove(m);
-		for (CoreferenceModelListener l : crModelListeners)
-			l.mentionRemoved(m);
-	}
-
-	public void fireMentionSelectedEvent(Mention m) {
-		for (CoreferenceModelListener l : crModelListeners)
-			l.annotationSelected(m);
-	}
-
-	public boolean addCoreferenceModelListener(CoreferenceModelListener e) {
-		return crModelListeners.add(e);
-	}
-
-	public boolean removeCoreferenceModelListener(Object o) {
-		return crModelListeners.remove(o);
+		// fire event
+		this.fireMentionChangedEvent(m);
 	}
 
 	@Override
 	public void valueChanged(TreeSelectionEvent e) {
 
-	}
-
-	public void removeMention(Mention m) {
-		CATreeNode parent = (CATreeNode) mentionMap.get(m).getParent();
-		int index = parent.getIndex(mentionMap.get(m));
-		parent.remove(mentionMap.get(m));
-		nodesWereRemoved(parent, new int[] { index }, new Object[] { mentionMap.get(m) });
-		fireMentionRemovedEvent(m);
-		mentionMap.remove(m);
-		m.removeFromIndexes();
 	}
 
 }
