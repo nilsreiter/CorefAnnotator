7fa4a75325d5306225a485510b42f3311b3a9c39
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-02-12 08:24:01 +0100

    full tokens is true by default

--- src/main/java/de/unistuttgart/ims/coref/annotator/Defaults.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/Defaults.java
@@ -1,7 +1,7 @@
 package de.unistuttgart.ims.coref.annotator;
 
 public class Defaults {
-	public static final boolean CFG_FULL_TOKENS = false;
+	public static final boolean CFG_FULL_TOKENS = true;
 	public static final boolean CFG_TRIM_WHITESPACE = true;
 	public static final boolean CFG_SHOW_TEXT_LABELS = true;
 	public static final String CFG_WINDOWTITLE = "de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData:documentTitle";
