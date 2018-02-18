0ecf2c8c310f9dc6bc62a16f0016da58ca11b0f2
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-02-18 15:05:57 +0100

    new component

---
+++ src/main/java/de/unistuttgart/ims/coref/annotator/comp/PanelList.java
@@ -0,0 +1,68 @@
+package de.unistuttgart.ims.coref.annotator.comp;
+
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.ListModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+
+import org.eclipse.collections.api.map.MutableMap;
+import org.eclipse.collections.impl.factory.Maps;
+
+import de.unistuttgart.ims.coref.annotator.Annotator;
+
+public class PanelList<T> extends JPanel implements ListDataListener {
+
+	private static final long serialVersionUID = 1L;
+
+	ListModel<T> model;
+	PanelFactory<T> factory;
+	MutableMap<T, JPanel> panelMap = Maps.mutable.empty();
+
+	public PanelList(PanelFactory<T> pFactory) {
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.factory = pFactory;
+		this.setPreferredSize(new Dimension(200, 300));
+	}
+
+	protected JPanel getPanel(T obj) {
+		if (!panelMap.containsKey(obj)) {
+			panelMap.put(obj, factory.getPanel(obj));
+		}
+		return panelMap.get(obj);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void intervalAdded(ListDataEvent e) {
+
+		Annotator.logger.debug("intervalAdded {}", e);
+		for (int i = e.getIndex0(); i <= e.getIndex1(); i++)
+			this.add(getPanel(((ListModel<T>) e.getSource()).getElementAt(i)), i);
+		revalidate();
+	}
+
+	@Override
+	public void intervalRemoved(ListDataEvent e) {
+		for (int i = e.getIndex0(); i < e.getIndex1(); i++)
+			this.remove(i);
+		revalidate();
+	}
+
+	@Override
+	public void contentsChanged(ListDataEvent e) {
+		revalidate();
+	}
+
+	public ListModel<T> getModel() {
+		return model;
+	}
+
+	public void setModel(ListModel<T> model) {
+		this.model = model;
+		model.addListDataListener(this);
+	}
+
+}
