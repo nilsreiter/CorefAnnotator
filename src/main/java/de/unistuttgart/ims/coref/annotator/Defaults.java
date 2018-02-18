88924291295db894181ebfeb4e750626f5119a0c
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-02-18 15:05:02 +0100

    Constants and keys

--- src/main/java/de/unistuttgart/ims/coref/annotator/Defaults.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/Defaults.java
@@ -9,5 +9,7 @@ public class Defaults {
 	public static final boolean CFG_SHOW_TEXT_LABELS = true;
 	public static final boolean CFG_TRIM_WHITESPACE = true;
 	public static final String CFG_WINDOWTITLE = "de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData:documentTitle";
+	public static final int CFG_SEARCH_RESULTS_CONTEXT = 50;
+	public static final String CFG_ANNOTATOR_ID = "Unknown annotator";
 
 }
