d7ae6f0423265610711ff0cb31fde30fef8a3c4b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-02-20 15:38:33 +0100

    Added CompoundIcon

--- src/main/java/de/unistuttgart/ims/coref/annotator/SearchPanel.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/SearchPanel.java
@@ -48,9 +48,10 @@ public class SearchPanel extends JFrame implements DocumentListener, WindowListe
 		private static final long serialVersionUID = 1L;
 
 		public AnnotateSelectedFindings() {
-			super(MaterialDesign.MDI_ACCOUNT_PLUS, Constants.Strings.ACTION_ADD_FINDINGS_TO_ENTITY);
+			super(MaterialDesign.MDI_ACCOUNT, Constants.Strings.ACTION_ADD_FINDINGS_TO_ENTITY);
 			putValue(Action.SHORT_DESCRIPTION,
 					Annotator.getString(Constants.Strings.ACTION_ADD_FINDINGS_TO_ENTITY_TOOLTIP));
+			this.addIkon(MaterialDesign.MDI_ARROW_RIGHT);
 		}
 
 		@Override
