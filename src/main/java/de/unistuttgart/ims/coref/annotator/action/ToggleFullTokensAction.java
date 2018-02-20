d54c92f44ddee31fefabe27d68d4eead90503b1f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-02-20 09:42:23 +0100

    inheriting new clas

--- src/main/java/de/unistuttgart/ims/coref/annotator/action/ToggleFullTokensAction.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/action/ToggleFullTokensAction.java
@@ -1,7 +1,5 @@
 package de.unistuttgart.ims.coref.annotator.action;
 
-import java.awt.event.ActionEvent;
-
 import javax.swing.Action;
 
 import org.kordamp.ikonli.materialdesign.MaterialDesign;
@@ -11,21 +9,13 @@ import de.unistuttgart.ims.coref.annotator.Constants;
 import de.unistuttgart.ims.coref.annotator.Constants.Strings;
 import de.unistuttgart.ims.coref.annotator.Defaults;
 
-public class ToggleFullTokensAction extends AnnotatorAction {
+public class ToggleFullTokensAction extends TogglePreferenceAction {
 	private static final long serialVersionUID = 1L;
 
 	public ToggleFullTokensAction(Annotator annotator) {
-		super(annotator, MaterialDesign.MDI_VIEW_WEEK, Strings.ACTION_TOGGLE_FULL_TOKENS);
+		super(annotator, MaterialDesign.MDI_VIEW_WEEK, Strings.ACTION_TOGGLE_FULL_TOKENS, Constants.CFG_FULL_TOKENS,
+				Defaults.CFG_FULL_TOKENS);
 		putValue(Action.SHORT_DESCRIPTION, Annotator.getString(Strings.ACTION_TOGGLE_FULL_TOKENS_TOOLTIP));
-		putValue(Action.SELECTED_KEY,
-				mainApplication.getPreferences().getBoolean(Constants.CFG_FULL_TOKENS, Defaults.CFG_FULL_TOKENS));
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		boolean old = mainApplication.getPreferences().getBoolean(Constants.CFG_FULL_TOKENS, Defaults.CFG_FULL_TOKENS);
-		mainApplication.getPreferences().putBoolean(Constants.CFG_FULL_TOKENS, !old);
-		putValue(Action.SELECTED_KEY, !old);
 	}
 
 }
