8a70b6569f259e4196f898282fe1253a710ca8bc
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-03-03 09:24:01 +0100

    No longer using deprecated class

--- src/main/java/de/unistuttgart/ims/coref/annotator/action/HelpAction.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/action/HelpAction.java
@@ -6,12 +6,12 @@ import org.kordamp.ikonli.materialdesign.MaterialDesign;
 
 import de.unistuttgart.ims.coref.annotator.HelpWindow;
 
-public class HelpAction extends IkonAction {
+public class HelpAction extends AnnotatorAction {
 
 	private static final long serialVersionUID = 1L;
 
 	public HelpAction() {
-		super("action.help", MaterialDesign.MDI_HELP);
+		super(null, "action.help", MaterialDesign.MDI_HELP);
 	}
 
 	@Override
