680284ed24194fca94acdc4e7317b1486c594be8
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-03-03 09:55:28 +0100

    more actions inherit from AnnotatorAction

--- src/main/java/de/unistuttgart/ims/coref/annotator/DocumentWindow.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/DocumentWindow.java
@@ -94,6 +94,7 @@ import org.kordamp.ikonli.materialdesign.MaterialDesign;
 import org.kordamp.ikonli.swing.FontIcon;
 
 import de.unistuttgart.ims.coref.annotator.Constants.Strings;
+import de.unistuttgart.ims.coref.annotator.action.AnnotatorAction;
 import de.unistuttgart.ims.coref.annotator.action.FileImportAction;
 import de.unistuttgart.ims.coref.annotator.action.FileOpenAction;
 import de.unistuttgart.ims.coref.annotator.action.FileSaveAction;
@@ -629,11 +630,11 @@ public class DocumentWindow extends JFrame implements CaretListener, TreeModelLi
 
 	}
 
-	class SetLanguageAction extends IkonAction {
+	class SetLanguageAction extends AnnotatorAction {
 		private static final long serialVersionUID = 1L;
 
 		public SetLanguageAction() {
-			super(Constants.Strings.ACTION_SET_DOCUMENT_LANGUAGE, MaterialDesign.MDI_SWITCH);
+			super(null, Constants.Strings.ACTION_SET_DOCUMENT_LANGUAGE, MaterialDesign.MDI_SWITCH);
 		}
 
 		@Override
@@ -1130,12 +1131,12 @@ public class DocumentWindow extends JFrame implements CaretListener, TreeModelLi
 
 	}
 
-	class ChangeKeyForEntityAction extends IkonAction {
+	class ChangeKeyForEntityAction extends AnnotatorAction {
 
 		private static final long serialVersionUID = 1L;
 
 		public ChangeKeyForEntityAction() {
-			super(Strings.ACTION_SET_SHORTCUT, MaterialDesign.MDI_KEYBOARD);
+			super(mainApplication, Strings.ACTION_SET_SHORTCUT, MaterialDesign.MDI_KEYBOARD);
 			putValue(Action.SHORT_DESCRIPTION, Annotator.getString(Strings.ACTION_SET_SHORTCUT_TOOLTIP));
 
 		}
@@ -1450,12 +1451,12 @@ public class DocumentWindow extends JFrame implements CaretListener, TreeModelLi
 
 	}
 
-	class MergeSelectedEntities extends IkonAction {
+	class MergeSelectedEntities extends AnnotatorAction {
 
 		private static final long serialVersionUID = 1L;
 
 		public MergeSelectedEntities() {
-			super(Strings.ACTION_MERGE, MaterialDesign.MDI_CALL_MERGE);
+			super(mainApplication, Strings.ACTION_MERGE, MaterialDesign.MDI_CALL_MERGE);
 			putValue(Action.SHORT_DESCRIPTION, Annotator.getString(Strings.ACTION_MERGE_TOOLTIP));
 		}
 
@@ -1471,11 +1472,11 @@ public class DocumentWindow extends JFrame implements CaretListener, TreeModelLi
 
 	}
 
-	class FormEntityGroup extends IkonAction {
+	class FormEntityGroup extends AnnotatorAction {
 		private static final long serialVersionUID = 1L;
 
 		public FormEntityGroup() {
-			super(Strings.ACTION_GROUP, MaterialDesign.MDI_GROUP);
+			super(mainApplication, Strings.ACTION_GROUP, MaterialDesign.MDI_GROUP);
 			putValue(Action.SHORT_DESCRIPTION, Annotator.getString(Strings.ACTION_GROUP_TOOLTIP));
 			putValue(Action.ACCELERATOR_KEY,
 					KeyStroke.getKeyStroke(KeyEvent.VK_G, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
