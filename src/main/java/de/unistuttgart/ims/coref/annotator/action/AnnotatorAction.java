7e85bf838301c7ca8eb6a6409572962499517534
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-03-03 09:23:33 +0100

    We can now control whether the string is a key or a string

--- src/main/java/de/unistuttgart/ims/coref/annotator/action/AnnotatorAction.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/action/AnnotatorAction.java
@@ -1,5 +1,7 @@
 package de.unistuttgart.ims.coref.annotator.action;
 
+import javax.swing.Action;
+
 import org.kordamp.ikonli.Ikon;
 
 import de.unistuttgart.ims.coref.annotator.Annotator;
@@ -19,6 +21,14 @@ public abstract class AnnotatorAction extends IkonAction {
 	public AnnotatorAction(Annotator mApp, String stringKey, Ikon... ikon) {
 		super(stringKey, ikon);
 		this.mainApplication = mApp;
+		putValue(Action.NAME, Annotator.getString(stringKey));
+
+	}
+
+	public AnnotatorAction(Annotator mApp, String stringKey, boolean isKey, Ikon... ikon) {
+		super(stringKey, ikon);
+		this.mainApplication = mApp;
+		putValue(Action.NAME, (isKey ? Annotator.getString(stringKey) : stringKey));
 
 	}
 
