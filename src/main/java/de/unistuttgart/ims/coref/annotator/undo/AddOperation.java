1afb8a1ae24247165166ef1fc13dee2f3024d9b4
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-03-16 15:51:29 +0100

    AddOperation

---
+++ src/main/java/de/unistuttgart/ims/coref/annotator/undo/AddOperation.java
@@ -0,0 +1,27 @@
+package de.unistuttgart.ims.coref.annotator.undo;
+
+import de.unistuttgart.ims.coref.annotator.api.Entity;
+import de.unistuttgart.ims.coref.annotator.api.Mention;
+
+public class AddOperation implements EditOperation {
+	Entity entity;
+	Mention mention;
+
+	public AddOperation(Entity entity, Mention mention) {
+		this.entity = entity;
+		this.mention = mention;
+	}
+
+	@Override
+	public String toString() {
+		return "add(" + mention.getBegin() + "," + mention.getEnd() + ")";
+	}
+
+	public Entity getEntity() {
+		return entity;
+	}
+
+	public Mention getMention() {
+		return mention;
+	}
+}
