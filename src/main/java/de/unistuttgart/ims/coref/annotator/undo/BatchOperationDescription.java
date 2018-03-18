34140a25add777c8646059acba9aa87bbf6891f5
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-03-18 12:01:44 +0100

    Updated Batch

--- src/main/java/de/unistuttgart/ims/coref/annotator/undo/BatchOperationDescription.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/undo/BatchOperationDescription.java
@@ -5,16 +5,20 @@ import java.util.Collection;
 import org.eclipse.collections.api.set.ImmutableSet;
 import org.eclipse.collections.impl.factory.Sets;
 
-public class BatchOperation implements EditOperationDescription {
+public class BatchOperationDescription implements EditOperationDescription {
 
 	ImmutableSet<EditOperationDescription> edits;
 
-	public BatchOperation(EditOperationDescription... edits) {
+	public BatchOperationDescription(EditOperationDescription... edits) {
 		this.edits = Sets.immutable.of(edits);
 	}
 
-	public BatchOperation(Collection<EditOperationDescription> edits) {
+	public BatchOperationDescription(Collection<EditOperationDescription> edits) {
 		this.edits = Sets.immutable.withAll(edits);
+	}
+
+	public ImmutableSet<EditOperationDescription> getEdits() {
+		return edits;
 	}
 
 }
