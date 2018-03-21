7ffc335b939836d332127dc34b2ffeb492175a75
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-03-21 13:53:33 +0100

    Added tests for entity moving

--- src/test/java/de/unistuttgart/ims/coref/annotator/document/TestCoreferenceModel.java
+++ src/test/java/de/unistuttgart/ims/coref/annotator/document/TestCoreferenceModel.java
@@ -2,6 +2,7 @@ package de.unistuttgart.ims.coref.annotator.document;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -12,7 +13,9 @@ import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.StringArray;
+import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.multimap.set.MutableSetMultimap;
+import org.eclipse.collections.impl.factory.Lists;
 import org.eclipse.collections.impl.factory.Sets;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -194,5 +197,38 @@ public class TestCoreferenceModel {
 		MutableSetMultimap<Entity, Mention> map = Sets.mutable.withAll(JCasUtil.select(jcas, Mention.class))
 				.groupBy(m -> m.getEntity());
 		assertEquals(2, map.keySet().size());
+	}
+
+	@Test
+	public void testEditMoveEntities() {
+		model.edit(new Op.AddMentionsToNewEntity(new Span(0, 1), new Span(2, 3)));
+		model.edit(new Op.AddMentionsToNewEntity(new Span(4, 5), new Span(6, 7)));
+		MutableList<Entity> entities = Lists.mutable.withAll(JCasUtil.select(jcas, Entity.class));
+
+		Mention m = ((Mention) model.getMentions(0).iterator().next());
+		assertNotEquals(entities.get(1), m.getEntity());
+		assertTrue(JCasUtil.exists(jcas, Mention.class));
+		assertTrue(JCasUtil.exists(jcas, Entity.class));
+		assertEquals(2, JCasUtil.select(jcas, Entity.class).size());
+		assertEquals(4, JCasUtil.select(jcas, Mention.class).size());
+		assertEquals(2, model.entityMentionMap.get(entities.get(1)).size());
+		assertEquals(2, model.entityMentionMap.get(entities.get(0)).size());
+
+		model.edit(new Op.MoveMentionsToEntity(entities.get(1), m));
+
+		assertEquals(entities.get(1), m.getEntity());
+		assertEquals(2, JCasUtil.select(jcas, Entity.class).size());
+		assertEquals(4, JCasUtil.select(jcas, Mention.class).size());
+		assertEquals(3, model.entityMentionMap.get(entities.get(1)).size());
+		assertEquals(1, model.entityMentionMap.get(entities.get(0)).size());
+
+		model.undo();
+
+		assertNotEquals(entities.get(1), m.getEntity());
+		assertEquals(2, JCasUtil.select(jcas, Entity.class).size());
+		assertEquals(4, JCasUtil.select(jcas, Mention.class).size());
+		assertEquals(2, model.entityMentionMap.get(entities.get(1)).size());
+		assertEquals(2, model.entityMentionMap.get(entities.get(0)).size());
+
 	}
 }
