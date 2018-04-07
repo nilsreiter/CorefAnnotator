a9efeee2777f66445b1b2194bcb3b1c5429ef1b0
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-04-07 21:45:49 +0200

    a new action to handle these new plugins

---
+++ src/main/java/de/unistuttgart/ims/coref/annotator/action/ProcessAction.java
@@ -0,0 +1,80 @@
+package de.unistuttgart.ims.coref.annotator.action;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.SwingWorker;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.eclipse.collections.api.multimap.MutableMultimap;
+import org.eclipse.collections.impl.factory.Multimaps;
+import org.kordamp.ikonli.materialdesign.MaterialDesign;
+
+import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
+import de.unistuttgart.ims.coref.annotator.Annotator;
+import de.unistuttgart.ims.coref.annotator.DocumentWindow;
+import de.unistuttgart.ims.coref.annotator.Span;
+import de.unistuttgart.ims.coref.annotator.document.Op;
+import de.unistuttgart.ims.coref.annotator.plugins.ProcessingPlugin;
+
+public class ProcessAction extends DocumentWindowAction {
+
+	private static final long serialVersionUID = 1L;
+
+	ProcessingPlugin plugin;
+
+	public ProcessAction(DocumentWindow dw, ProcessingPlugin plugin) {
+		super(dw, plugin.getName(), false, MaterialDesign.MDI_AUTO_FIX);
+		this.plugin = plugin;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		Runnable r = new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					documentWindow.setIndeterminateProgress();
+					JCas jcas = documentWindow.getJCas();
+
+					SimplePipeline.runPipeline(jcas, plugin.getEngineDescription());
+
+					MutableMultimap<String, Span> map = Multimaps.mutable.set.empty();
+					for (NamedEntity ne : JCasUtil.select(jcas, NamedEntity.class)) {
+						map.put(ne.getCoveredText(), new Span(ne));
+					}
+
+					for (String surface : map.keySet()) {
+						Op op = new Op.AddMentionsToNewEntity(map.get(surface));
+						documentWindow.getDocumentModel().getCoreferenceModel().edit(op);
+					}
+
+					documentWindow.stopIndeterminateProgress();
+				} catch (AnalysisEngineProcessException | ResourceInitializationException e) {
+					Annotator.logger.catching(e);
+				} finally {
+					documentWindow.stopIndeterminateProgress();
+				}
+
+			}
+
+		};
+
+		new Thread(r).start();
+	}
+
+	class Perform extends SwingWorker<JCas, Object> {
+
+		@Override
+		protected JCas doInBackground() throws Exception {
+			return null;
+		}
+
+	}
+
+}
