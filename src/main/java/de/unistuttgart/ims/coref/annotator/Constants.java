2c38790967b6a1dd24d6fb1446f74ab3f64f130b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-04-07 21:47:13 +0200

    sorting

--- src/main/java/de/unistuttgart/ims/coref/annotator/Constants.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/Constants.java
@@ -91,6 +91,8 @@ public class Constants {
 		public static final String ACTION_DELETE_TOOLTIP = "action.delete.tooltip";
 		public static final String ACTION_EDIT_COMMENT = "action.edit.comment";
 		public static final String ACTION_EDIT_COMMENT_TOOLTIP = "action.edit.comment.tooltip";
+		public static final String ACTION_FILE_MERGE = "action.merge_files";
+		public static final String ACTION_FILE_MERGE_TOOLTIP = "action.merge_files.tooltip";
 		public static final String ACTION_FLAG_ENTITY_GENERIC = "action.flag_entity_generic";
 		public static final String ACTION_FLAG_ENTITY_GENERIC_TOOLTIP = "action.flag_entity_generic.tooltip";
 		public static final String ACTION_FLAG_MENTION_AMBIGUOUS = "action.flag_mention_ambiguous";
@@ -213,8 +215,6 @@ public class Constants {
 		public static final String STATUS_STYLE = "status.style";
 		public static final String WINDOWTITLE_EDITED = "windowtitle.edited";
 		public static final String WINDOWTITLE_NEW_FILE = "windowtitle.new_file";
-		public static final String ACTION_FILE_MERGE = "action.merge_files";
-		public static final String ACTION_FILE_MERGE_TOOLTIP = "action.merge_files.tooltip";
 
 	}
 
