4fff57c9d9d8de7788f43cf5154e5cfd8d5f9426
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-04-09 10:44:20 +0200

    Help page about IO plugins

--- src/main/java/de/unistuttgart/ims/coref/annotator/HelpWindow.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/HelpWindow.java
@@ -10,6 +10,10 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
+import org.apache.commons.io.IOUtils;
+
+import de.unistuttgart.ims.coref.annotator.plugins.IOPlugin;
+
 public class HelpWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -25,6 +29,7 @@ public class HelpWindow extends JFrame {
 		tabbedPane.addTab("How to annotate", new JScrollPane(load("docs/howto")));
 		tabbedPane.addTab("Compare annotations", new JScrollPane(load("docs/compare")));
 		tabbedPane.addTab("Automatic processing", new JScrollPane(load("docs/processing")));
+		tabbedPane.addTab("Input/Output", new JScrollPane(loadIOPlugins()));
 
 		this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		this.pack();
@@ -48,6 +53,38 @@ public class HelpWindow extends JFrame {
 			Annotator.logger.catching(e);
 		}
 		return null;
+	}
+
+	private JEditorPane loadIOPlugins() {
+		JEditorPane textArea = new JEditorPane();
+		textArea.setContentType("text/html");
+
+		StringBuilder b = new StringBuilder();
+
+		try {
+			b.append(IOUtils.toString(getClass().getResourceAsStream("/docs/io-plugins.txt"), "UTF-8"));
+		} catch (IOException e) {
+			b.append("<html><head><title>Input/Output</title></head>");
+			b.append("<body style=\"font-family:sans-serif;font-size:12pt;\"><h1>Input/Output</h1>");
+		}
+
+		for (Class<? extends IOPlugin> pluginClass : Annotator.app.getPluginManager().getIOPlugins()) {
+			IOPlugin instance = Annotator.app.getPluginManager().getIOPlugin(pluginClass);
+
+			b.append("<h2>").append(instance.getName()).append("</h2>");
+			b.append("<p>Class name: <code>").append(pluginClass.getName()).append("</code></p>");
+			if (instance.getDescription() == null)
+				b.append("<p>No description.</p>");
+			else
+				b.append("<p>").append(instance.getDescription()).append("</p>");
+		}
+		b.append("</body></html>");
+
+		textArea.setText(b.toString());
+		textArea.setEditable(false);
+		textArea.setPreferredSize(new Dimension(500, 500));
+
+		return textArea;
 	}
 
 	public static HelpWindow getHelpWindow() {
