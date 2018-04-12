1ccc93c9f9451ab3e4390c6ea4a03faeb16a77a5
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-04-12 17:48:55 +0200

    Using BreakIteratorSegmenter and adding supported languages

--- src/main/java/de/unistuttgart/ims/coref/annotator/plugin/proc/stanford/NerPlugin.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/plugin/proc/stanford/NerPlugin.java
@@ -6,7 +6,7 @@ import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
-import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 import de.unistuttgart.ims.coref.annotator.plugins.ProcessingPlugin;
 
 public class NerPlugin implements ProcessingPlugin {
@@ -24,16 +24,15 @@ public class NerPlugin implements ProcessingPlugin {
 	@Override
 	public AnalysisEngineDescription getEngineDescription() throws ResourceInitializationException {
 		AggregateBuilder b = new AggregateBuilder();
-		b.add(AnalysisEngineFactory.createEngineDescription(StanfordSegmenter.class));
+		b.add(AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class));
 		b.add(AnalysisEngineFactory.createEngineDescription(StanfordNamedEntityRecognizer.class));
-		// b.add(AnalysisEngineFactory.createEngineDescription(ClearAnnotation.class,
-		// ClearAnnotation.PARAM_TYPE));
+
 		return b.createAggregateDescription();
 	}
 
 	@Override
 	public String[] getSupportedLanguages() {
-		return new String[] { "en", "de" };
+		return new String[] { "en", "de", "es", "fr", "it", "nl", "ru" };
 	}
 
 }
