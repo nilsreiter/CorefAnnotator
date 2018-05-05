6b464d63ad3d16e08c04f1583bcc71d1ebfb9d84
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-05 23:00:38 +0200

    added static method to look up the current version

--- src/main/java/de/unistuttgart/ims/coref/annotator/TypeSystemVersion.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/TypeSystemVersion.java
@@ -1,5 +1,9 @@
 package de.unistuttgart.ims.coref.annotator;
 
 public enum TypeSystemVersion {
-	LEGACY, v1_0
+	LEGACY, v1_0;
+
+	static TypeSystemVersion getCurrent() {
+		return TypeSystemVersion.v1_0;
+	}
 }
