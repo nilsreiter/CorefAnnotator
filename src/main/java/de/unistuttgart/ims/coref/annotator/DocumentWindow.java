9954b2a06a99e76c2b5f386217eea7883229f847
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-10 12:42:29 +0200

    Updated versioned type system

--- src/main/java/de/unistuttgart/ims/coref/annotator/DocumentWindow.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/DocumentWindow.java
@@ -134,6 +134,7 @@ import de.unistuttgart.ims.coref.annotator.api.v1.DetachedMentionPart;
 import de.unistuttgart.ims.coref.annotator.api.v1.Entity;
 import de.unistuttgart.ims.coref.annotator.api.v1.EntityGroup;
 import de.unistuttgart.ims.coref.annotator.api.v1.Mention;
+import de.unistuttgart.ims.coref.annotator.comp.SegmentIndicator;
 import de.unistuttgart.ims.coref.annotator.document.CoreferenceModel;
 import de.unistuttgart.ims.coref.annotator.document.DocumentModel;
 import de.unistuttgart.ims.coref.annotator.document.DocumentState;
