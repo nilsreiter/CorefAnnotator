bd606efb324038e5e413b0ca5f3de06913a90590
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-22 17:54:34 +0200

    this is now public

--- src/main/java/de/unistuttgart/ims/coref/annotator/TypeSystemVersion.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/TypeSystemVersion.java
@@ -3,7 +3,7 @@ package de.unistuttgart.ims.coref.annotator;
 public enum TypeSystemVersion {
 	LEGACY, v1;
 
-	static TypeSystemVersion getCurrent() {
+	public static TypeSystemVersion getCurrent() {
 		return TypeSystemVersion.v1;
 	}
 }
