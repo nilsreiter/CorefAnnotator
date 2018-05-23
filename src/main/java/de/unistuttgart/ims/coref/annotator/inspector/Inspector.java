667c03e46308bd2d31e5ab2fe81f0fa6191e0225
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-23 22:44:58 +0200

    Need to use the panel list for reasons ...

--- src/main/java/de/unistuttgart/ims/coref/annotator/inspector/Inspector.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/inspector/Inspector.java
@@ -4,24 +4,24 @@ import java.awt.BorderLayout;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JDialog;
-import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListModel;
 
 import de.unistuttgart.ims.coref.annotator.DocumentWindow;
+import de.unistuttgart.ims.coref.annotator.comp.PanelList;
 
 public class Inspector extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 
-	JList<Issue> issueList;
+	PanelList<Issue, JPanel> issueList;
 
 	public Inspector(DocumentWindow dw) {
 		super(dw);
 
 		DefaultListModel<Issue> listModel = new DefaultListModel<Issue>();
-		issueList = new JList<Issue>();
-		issueList.setCellRenderer(new IssuePanelFactory());
+		issueList = new PanelList<Issue, JPanel>(new IssuePanelFactory());
 
 		Checker checker = new Checker(dw.getDocumentModel(), this, listModel);
 		checker.execute();
