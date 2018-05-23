3233b4d38f9c30dbf9bcdccabd05e4db6a5c6685
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-23 22:13:33 +0200

    we now use a regular JList instead of a Panel list

--- src/main/java/de/unistuttgart/ims/coref/annotator/inspector/Inspector.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/inspector/Inspector.java
@@ -1,43 +1,40 @@
 package de.unistuttgart.ims.coref.annotator.inspector;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
+import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.ListModel;
 
 import de.unistuttgart.ims.coref.annotator.DocumentWindow;
-import de.unistuttgart.ims.coref.annotator.comp.PanelList;
 
 public class Inspector extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 
-	PanelList<Issue, JPanel> issueList;
+	JList<Issue> issueList;
 
 	public Inspector(DocumentWindow dw) {
 		super(dw);
-		setPreferredSize(new Dimension(300, 600));
 
 		DefaultListModel<Issue> listModel = new DefaultListModel<Issue>();
-		issueList = new PanelList<Issue, JPanel>(new IssuePanelFactory());
+		issueList = new JList<Issue>();
+		issueList.setCellRenderer(new IssuePanelFactory());
 
-		listModel.addListDataListener(issueList);
 		Checker checker = new Checker(dw.getDocumentModel(), this, listModel);
 		checker.execute();
 
 		getContentPane().add(new JScrollPane(issueList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), BorderLayout.CENTER);
-		getContentPane().setPreferredSize(new Dimension(300, 600));
 
 		setModalityType(ModalityType.MODELESS);
 
 	}
 
 	public void setListModel(ListModel<Issue> listModel) {
+		issueList.setModel(listModel);
 		setVisible(true);
 		pack();
 	}
