1d906962029ee683b659f65ab6bf3a1685e478cd
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-23 17:00:44 +0200

    javadoc

--- src/main/java/de/unistuttgart/ims/coref/annotator/plugins/Plugin.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/plugins/Plugin.java
@@ -1,8 +1,27 @@
 package de.unistuttgart.ims.coref.annotator.plugins;
 
+/**
+ * All plugins should implement this interface to provide some meta data for the
+ * UI.
+ * 
+ * @author reiterns
+ *
+ */
 public interface Plugin {
+
+	/**
+	 * Returns a description of the plugin, to be shown as a tooltip and/or in the
+	 * help menu.
+	 * 
+	 * @return
+	 */
 	String getDescription();
 
+	/**
+	 * A label for buttons and menu entries
+	 * 
+	 * @return
+	 */
 	String getName();
 
 }
