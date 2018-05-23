0feda1ce7611e99c1ffafa6349471183b00da394
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-23 18:34:21 +0200

    fix for #119

--- src/main/java/de/unistuttgart/ims/coref/annotator/Constants.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/Constants.java
@@ -236,7 +236,6 @@ public class Constants {
 	public static final String CFG_SHOW_TEXT_LABELS = "Show text labels";
 	public static final String CFG_TRIM_WHITESPACE = "Trim Whitespace";
 
-	public static final String CFG_WINDOWTITLE = "Windowtitle";
 	public static final String ENTITY_FLAG_GENERIC = "Generic";
 	public static final String ENTITY_FLAG_HIDDEN = "Hidden";
 
