29b69d1b80ee215848598dbdfe3eeb13414b2e69
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-06-08 08:56:43 +0200

    We probably need a flag model

---
+++ src/main/java/de/unistuttgart/ims/coref/annotator/document/FlagModel.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.ims.coref.annotator.document;
+
+import java.util.prefs.Preferences;
+
+public class FlagModel {
+	DocumentModel documentModel;
+
+	public FlagModel(DocumentModel documentModel, Preferences preferences) {
+
+	}
+
+}
