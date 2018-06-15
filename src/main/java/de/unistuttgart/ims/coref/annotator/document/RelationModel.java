216730f9d6b22ba00764a708cd8c806dcb9d5b3d
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-06-15 17:58:08 +0200

    initial relation model

---
+++ src/main/java/de/unistuttgart/ims/coref/annotator/document/RelationModel.java
@@ -0,0 +1,19 @@
+package de.unistuttgart.ims.coref.annotator.document;
+
+import org.apache.uima.fit.util.JCasUtil;
+import org.eclipse.collections.api.set.ImmutableSet;
+import org.eclipse.collections.impl.factory.Sets;
+
+import de.unistuttgart.ims.coref.annotator.api.v1.EntityRelation;
+
+public class RelationModel {
+	DocumentModel documentModel;
+
+	public RelationModel(DocumentModel documentModel) {
+		this.documentModel = documentModel;
+	}
+
+	public ImmutableSet<EntityRelation> getRelations() {
+		return Sets.immutable.withAll(JCasUtil.select(documentModel.getJcas(), EntityRelation.class));
+	}
+}
