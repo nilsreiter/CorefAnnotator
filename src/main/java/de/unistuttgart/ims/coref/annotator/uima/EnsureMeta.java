6202bdbcc0f5aac5c60005015ad53c2ff0d77a05
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-07-11 11:29:25 +0200

    EnsureMeta now also sets the ts version and style plugin

--- src/main/java/de/unistuttgart/ims/coref/annotator/uima/EnsureMeta.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/uima/EnsureMeta.java
@@ -2,17 +2,26 @@ package de.unistuttgart.ims.coref.annotator.uima;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
+import de.unistuttgart.ims.coref.annotator.Annotator;
 import de.unistuttgart.ims.coref.annotator.api.Meta;
 
 public class EnsureMeta extends JCasAnnotator_ImplBase {
+
+	public static final String PARAM_TS_VERSION = "TS_VERSION";
+
+	@ConfigurationParameter(name = PARAM_TS_VERSION, mandatory = false, defaultValue = "v1")
+	String typeSystemVersion;
 
 	@Override
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
 		if (!JCasUtil.exists(aJCas, Meta.class)) {
 			Meta meta = new Meta(aJCas);
+			meta.setTypeSystemVersion(typeSystemVersion);
+			meta.setStylePlugin(Annotator.app.getPluginManager().getDefaultStylePlugin().getClass().getName());
 			meta.addToIndexes();
 		}
 
