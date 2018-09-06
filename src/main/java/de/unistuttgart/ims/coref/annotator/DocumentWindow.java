de26bd597b53830ca2dd31fbd9394ebeaec2a450
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-09-06 08:41:57 +0200

    Removed the key listener and added the new action via ActionMap. Fixes #141

--- src/main/java/de/unistuttgart/ims/coref/annotator/DocumentWindow.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/DocumentWindow.java
@@ -96,6 +96,7 @@ import org.kordamp.ikonli.swing.FontIcon;
 
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import de.unistuttgart.ims.coref.annotator.Constants.Strings;
+import de.unistuttgart.ims.coref.annotator.action.AddCurrentSpanToCurrentEntity;
 import de.unistuttgart.ims.coref.annotator.action.ChangeColorForEntity;
 import de.unistuttgart.ims.coref.annotator.action.CopyAction;
 import de.unistuttgart.ims.coref.annotator.action.DeleteAction;
@@ -257,7 +258,9 @@ public class DocumentWindow extends AbstractWindow implements CaretListener, Tre
 		tree.setCellRenderer(new MyTreeCellRenderer());
 		tree.addTreeSelectionListener(new MyTreeSelectionListener(tree));
 		tree.addMouseListener(new TreeMouseListener());
-		tree.addKeyListener(treeKeyListener);
+
+		tree.getInputMap().put(KeyStroke.getKeyStroke("ENTER"), AddCurrentSpanToCurrentEntity.class);
+		tree.getActionMap().put(AddCurrentSpanToCurrentEntity.class, new AddCurrentSpanToCurrentEntity(this));
 
 		treeSearchField = new JTextField();
 		EntityFinder entityFinder = new EntityFinder();
