b387e6119cf672901f28ad23b8b26e67a29acfef
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-02-01 11:36:19 +0100

    DeleteFlag now stores feature structures in which the flag was active

--- src/main/java/de/unistuttgart/ims/coref/annotator/document/op/DeleteFlag.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/document/op/DeleteFlag.java
@@ -1,9 +1,13 @@
 package de.unistuttgart.ims.coref.annotator.document.op;
 
+import org.apache.uima.cas.FeatureStructure;
+import org.eclipse.collections.api.list.ImmutableList;
+
 import de.unistuttgart.ims.coref.annotator.api.v1.Flag;
 
 public class DeleteFlag implements FlagModelOperation {
 	Flag flag;
+	ImmutableList<FeatureStructure> featureStructures;
 
 	public DeleteFlag(Flag flag) {
 		this.flag = flag;
@@ -15,5 +19,13 @@ public class DeleteFlag implements FlagModelOperation {
 
 	public void setFlag(Flag flag) {
 		this.flag = flag;
+	}
+
+	public ImmutableList<FeatureStructure> getFeatureStructures() {
+		return featureStructures;
+	}
+
+	public void setFeatureStructures(ImmutableList<FeatureStructure> mentions) {
+		this.featureStructures = mentions;
 	}
 }
