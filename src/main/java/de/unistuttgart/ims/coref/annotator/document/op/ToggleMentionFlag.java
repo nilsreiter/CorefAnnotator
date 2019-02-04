c6b3bee799a8093e74a9013da83d4d606d676f35
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-02-04 17:02:40 +0100

    type specific toggle flag operations are now deprecated

--- src/main/java/de/unistuttgart/ims/coref/annotator/document/op/ToggleMentionFlag.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/document/op/ToggleMentionFlag.java
@@ -2,6 +2,7 @@ package de.unistuttgart.ims.coref.annotator.document.op;
 
 import de.unistuttgart.ims.coref.annotator.api.v1.Mention;
 
+@Deprecated
 public class ToggleMentionFlag extends ToggleFlag<Mention> {
 
 	public ToggleMentionFlag(String flag, Iterable<Mention> objects) {
