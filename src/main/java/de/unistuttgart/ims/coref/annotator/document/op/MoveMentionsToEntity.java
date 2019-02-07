b8b2805062f3c1fd11caa13f3fb1c54bba6607fd
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-02-07 18:36:58 +0100

    Renamed class

--- src/main/java/de/unistuttgart/ims/coref/annotator/document/op/MoveMentionsToEntity.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/document/op/MoveMentionsToEntity.java
@@ -5,7 +5,7 @@ import org.eclipse.collections.api.list.ImmutableList;
 import de.unistuttgart.ims.coref.annotator.api.v1.Entity;
 import de.unistuttgart.ims.coref.annotator.api.v1.Mention;
 
-public class MoveMentionsToEntity extends MoveOp<Mention, Entity> {
+public class MoveMentionsToEntity extends MoveOperation<Mention, Entity> {
 
 	public MoveMentionsToEntity(Entity target, Iterable<Mention> mention) {
 		super(mention.iterator().next().getEntity(), target, mention);
