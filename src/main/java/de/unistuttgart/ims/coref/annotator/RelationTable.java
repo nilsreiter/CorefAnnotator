15f2b7f6c1039feb7f0fc6c706b928318472520b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-02-10 13:14:25 +0100

    deprecated

--- src/main/java/de/unistuttgart/ims/coref/annotator/RelationTable.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/RelationTable.java
@@ -6,6 +6,7 @@ import javax.swing.JTable;
 
 import de.unistuttgart.ims.coref.annotator.document.DocumentModel;
 
+@Deprecated
 public class RelationTable extends JTable {
 
 	private static final long serialVersionUID = 1L;
