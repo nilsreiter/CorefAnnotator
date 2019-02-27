48a18cf8b5f43f677a0010dc51a605f615fa7a29
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-02-27 08:02:24 +0100

    CSV export of mentions now takes into account user-created mention flags

--- src/main/java/de/unistuttgart/ims/coref/annotator/action/EntityStatisticsAction.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/action/EntityStatisticsAction.java
@@ -10,17 +10,20 @@ import javax.swing.SwingWorker;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import org.eclipse.collections.api.list.ImmutableList;
 import org.kordamp.ikonli.materialdesign.MaterialDesign;
 
 import de.unistuttgart.ims.coref.annotator.Annotator;
 import de.unistuttgart.ims.coref.annotator.Constants;
 import de.unistuttgart.ims.coref.annotator.Constants.Strings;
-import de.unistuttgart.ims.coref.annotator.api.v1.Entity;
-import de.unistuttgart.ims.coref.annotator.api.v1.EntityGroup;
-import de.unistuttgart.ims.coref.annotator.api.v1.Mention;
 import de.unistuttgart.ims.coref.annotator.DocumentWindow;
 import de.unistuttgart.ims.coref.annotator.FileFilters;
 import de.unistuttgart.ims.coref.annotator.Util;
+import de.unistuttgart.ims.coref.annotator.api.v1.Entity;
+import de.unistuttgart.ims.coref.annotator.api.v1.EntityGroup;
+import de.unistuttgart.ims.coref.annotator.api.v1.Flag;
+import de.unistuttgart.ims.coref.annotator.api.v1.Mention;
+import de.unistuttgart.ims.coref.annotator.document.FlagModel;
 
 public class EntityStatisticsAction extends DocumentWindowAction {
 
@@ -49,9 +52,21 @@ public class EntityStatisticsAction extends DocumentWindowAction {
 				protected Object doInBackground() throws Exception {
 					getDocumentWindow().setMessage("Exporting ...");
 					getDocumentWindow().setIndeterminateProgress();
+					FlagModel flagModel = getDocumentWindow().getDocumentModel().getFlagModel();
+					ImmutableList<Flag> mentionFlags = flagModel.getFlags()
+							.select(f -> f.getTargetClass().equalsIgnoreCase(Mention.class.getName()));
 					try (CSVPrinter p = new CSVPrinter(new FileWriter(chooser.getSelectedFile()), CSVFormat.EXCEL)) {
-						p.printRecord("begin", "end", "surface", "entityNum", "entityLabel", "entityGroup",
-								"entityGeneric", "ambiguous", "difficult");
+						p.print("begin");
+						p.print("end");
+						p.print("surface");
+						p.print("entityNum");
+						p.print("entityLabel");
+						p.print("entityGroup");
+						p.print("entityGeneric");
+						for (Flag flag : mentionFlags) {
+							p.print(flag.getLabel());
+						}
+						p.println();
 						int entityNum = 0;
 						for (Entity entity : getDocumentWindow().getSelectedEntities()) {
 							for (Mention mention : getDocumentWindow().getDocumentModel().getCoreferenceModel()
@@ -59,9 +74,17 @@ public class EntityStatisticsAction extends DocumentWindowAction {
 								String surface = mention.getCoveredText();
 								if (mention.getDiscontinuous() != null)
 									surface += " " + mention.getDiscontinuous().getCoveredText();
-								p.printRecord(mention.getBegin(), mention.getEnd(), surface, entityNum,
-										entity.getLabel(), (entity instanceof EntityGroup), Util.isGeneric(entity),
-										Util.isAmbiguous(mention), Util.isDifficult(mention));
+								p.print(mention.getBegin());
+								p.print(mention.getEnd());
+								p.print(surface);
+								p.print(entityNum);
+								p.print(entity.getLabel());
+								p.print((entity instanceof EntityGroup));
+								p.print(Util.isGeneric(entity));
+								for (Flag flag : mentionFlags) {
+									p.print(Util.isX(mention, flag.getKey()));
+								}
+								p.println();
 							}
 							entityNum++;
 						}
