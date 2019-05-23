0940fdcf8afc5533b9f145b9694cb25ae6d961e3
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-05-23 09:48:54 +0200

    entity labels are now abbreviated if needed

--- src/main/java/de/unistuttgart/ims/coref/annotator/comp/EntityLabel.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/comp/EntityLabel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 import javax.swing.JLabel;
 
+import org.apache.commons.lang3.StringUtils;
 import org.kordamp.ikonli.materialdesign.MaterialDesign;
 import org.kordamp.ikonli.swing.FontIcon;
 
@@ -14,10 +15,12 @@ public class EntityLabel extends JLabel {
 
 	private static final long serialVersionUID = 1L;
 
+	int maxLength = 15;
+
 	public EntityLabel(Entity entity) {
 		Color entityColor = new Color(entity.getColor());
 
-		this.setText(entity.getLabel());
+		this.setText(StringUtils.abbreviate(entity.getLabel(), maxLength));
 		if (entity instanceof EntityGroup) {
 			this.setIcon(FontIcon.of(MaterialDesign.MDI_ACCOUNT_MULTIPLE, entityColor));
 		} else {
