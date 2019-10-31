ab7c17f27babf6e6684880fe311b424415d8fb95
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-10-31 18:11:16 +0100

    updated code to reduce warnings

--- src/main/java/de/unistuttgart/ims/coref/annotator/document/EntityTreeModel.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/document/EntityTreeModel.java
@@ -74,7 +74,7 @@ public class EntityTreeModel extends DefaultTreeModel implements CoreferenceMode
 
 	@Override
 	public void entityEvent(FeatureStructureEvent event) {
-		Annotator.logger.entry(event);
+		Annotator.logger.traceEntry();
 		Event.Type eventType = event.getType();
 		switch (eventType) {
 		case Add:
