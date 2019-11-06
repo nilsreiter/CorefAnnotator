29f74116b698f379f3b59edc78b53637acb405bd
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-11-06 13:28:14 +0100

    Export entity statistics now allows some config

--- src/main/java/de/unistuttgart/ims/coref/annotator/action/EntityStatisticsAction.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/action/EntityStatisticsAction.java
@@ -1,21 +1,37 @@
 package de.unistuttgart.ims.coref.annotator.action;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.function.Consumer;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.collections.api.list.ImmutableList;
 import org.kordamp.ikonli.materialdesign.MaterialDesign;
 
 import de.unistuttgart.ims.coref.annotator.Annotator;
 import de.unistuttgart.ims.coref.annotator.DocumentWindow;
 import de.unistuttgart.ims.coref.annotator.FileFilters;
+import de.unistuttgart.ims.coref.annotator.HelpWindow;
 import de.unistuttgart.ims.coref.annotator.Strings;
 import de.unistuttgart.ims.coref.annotator.Util;
 import de.unistuttgart.ims.coref.annotator.api.v1.Entity;
@@ -32,15 +48,87 @@ public class EntityStatisticsAction extends DocumentWindowAction {
 	private static final String SURFACE = "surface";
 	private static final String END = "end";
 	private static final String BEGIN = "begin";
+	private static final String CONTEXT_LEFT = "leftContext";
+	private static final String CONTEXT_RIGHT = "rightContext";
 
 	private static final long serialVersionUID = 1L;
+
+	// initial option values
+	int optionContextWidth = 10;
+	boolean optionTrimWhitespace = true;
 
 	public EntityStatisticsAction(DocumentWindow dw) {
 		super(dw, Strings.ACTION_ENTITY_STATISTICS, MaterialDesign.MDI_CHART_BAR);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
+	protected void optionDialog(Consumer<EntityStatisticsAction> callback) {
+
+		JSpinner spinner = new JSpinner(new SpinnerNumberModel(optionContextWidth, 0, 100, 5));
+		JCheckBox trimWhitespace = new JCheckBox();
+		trimWhitespace.setSelected(optionTrimWhitespace);
+
+		JDialog dialog = new JDialog(getTarget(), Annotator.getString(Strings.DIALOG_EXPORT_OPTIONS_TITLE));
+
+		JPanel optionPanel = new JPanel(new GridLayout(0, 2));
+		optionPanel.add(getLabel(Annotator.getString("dialog.export_options.context_width"),
+				Annotator.getString("dialog.export_options.context_width.tooltip")));
+		optionPanel.add(spinner);
+
+		optionPanel.add(getLabel(Annotator.getString("dialog.export_options.trim_whitespace"),
+				Annotator.getString("dialog.export_options.trim_whitespace.tooltip")));
+		optionPanel.add(trimWhitespace);
+
+		optionPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+
+		Action okAction = new AbstractAction(Annotator.getString(Strings.DIALOG_EXPORT_OPTIONS_OK)) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				optionContextWidth = ((SpinnerNumberModel) spinner.getModel()).getNumber().intValue();
+				optionTrimWhitespace = trimWhitespace.isSelected();
+				dialog.dispose();
+				callback.accept(EntityStatisticsAction.this);
+			}
+		};
+
+		Action cancelAction = new AbstractAction(
+				Annotator.getString(de.unistuttgart.ims.coref.annotator.Strings.DIALOG_CANCEL)) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dialog.dispose();
+			}
+		};
+
+		Action helpAction = new AbstractAction(
+				Annotator.getString(de.unistuttgart.ims.coref.annotator.Strings.MENU_HELP)) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				HelpWindow.show("Input/Output");
+			}
+		};
+
+		JButton okButton = new JButton(okAction);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(okButton);
+		buttonPanel.add(new JButton(cancelAction));
+		buttonPanel.add(new JButton(helpAction));
+
+		dialog.getContentPane().add(optionPanel, BorderLayout.CENTER);
+		dialog.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+		dialog.pack();
+		dialog.setLocationRelativeTo(getTarget());
+		dialog.setVisible(true);
+		SwingUtilities.getRootPane(okButton).setDefaultButton(okButton);
+		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+	}
+
+	protected void saveDialog() {
 		JFileChooser chooser = new JFileChooser(Annotator.app.getCurrentDirectory());
 		chooser.setDialogType(JFileChooser.SAVE_DIALOG);
 		chooser.setFileFilter(FileFilters.csv);
@@ -64,11 +152,19 @@ public class EntityStatisticsAction extends DocumentWindowAction {
 					ImmutableList<Flag> entityFlags = flagModel.getFlags()
 							.select(f -> f.getTargetClass().equalsIgnoreCase(Entity.class.getName()));
 
+					String text = getDocumentWindow().getDocumentModel().getJcas().getDocumentText();
+
 					try (CSVPrinter p = new CSVPrinter(new FileWriter(chooser.getSelectedFile()), CSVFormat.EXCEL)) {
 						// this is the header row
 						p.print(BEGIN);
 						p.print(END);
+						if (optionContextWidth > 0) {
+							p.print(CONTEXT_LEFT);
+						}
 						p.print(SURFACE);
+						if (optionContextWidth > 0) {
+							p.print(CONTEXT_RIGHT);
+						}
 						p.print(ENTITY_NUM);
 						p.print(ENTITY_LABEL);
 						p.print(ENTITY_GROUP);
@@ -88,7 +184,28 @@ public class EntityStatisticsAction extends DocumentWindowAction {
 									surface += " " + mention.getDiscontinuous().getCoveredText();
 								p.print(mention.getBegin());
 								p.print(mention.getEnd());
-								p.print(surface);
+								if (optionContextWidth > 0) {
+									String lc;
+									if (optionTrimWhitespace) {
+										lc = StringUtils.right(text.substring(0, mention.getBegin()).trim(),
+												optionContextWidth);
+
+									} else {
+										lc = StringUtils.right(text, optionContextWidth);
+									}
+									p.print(lc);
+								}
+								p.print((optionTrimWhitespace ? surface.trim() : surface));
+								if (optionContextWidth > 0) {
+									String rc;
+									if (optionTrimWhitespace) {
+										rc = StringUtils.left(text.substring(mention.getEnd()).trim(),
+												optionContextWidth);
+									} else {
+										rc = StringUtils.left(text, optionContextWidth);
+									}
+									p.print(rc);
+								}
 								p.print(entityNum);
 								p.print(entity.getLabel());
 								p.print((entity instanceof EntityGroup));
@@ -116,7 +233,17 @@ public class EntityStatisticsAction extends DocumentWindowAction {
 			}.execute();
 
 		}
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		optionDialog(a -> a.saveDialog());
+	}
+
+	protected JLabel getLabel(String text, String tooltip) {
+		JLabel lab = new JLabel(text);
+		lab.setToolTipText(tooltip);
+		return lab;
 	}
 
 }
