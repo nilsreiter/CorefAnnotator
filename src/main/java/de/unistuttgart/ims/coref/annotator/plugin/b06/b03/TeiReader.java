c479de7c4ad48c62fb492d6c62ae0d32152ff3bd
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-11-06 14:02:36 +0100

    initial implementation of a specific B06-B03 importer

---
+++ src/main/java/de/unistuttgart/ims/coref/annotator/plugin/b06/b03/TeiReader.java
@@ -0,0 +1,111 @@
+package de.unistuttgart.ims.coref.annotator.plugin.b06.b03;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.eclipse.collections.api.map.MutableMap;
+import org.eclipse.collections.impl.factory.Maps;
+import org.eclipse.collections.impl.factory.Sets;
+
+import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.CompressionUtils;
+import de.unistuttgart.ims.coref.annotator.Annotator;
+import de.unistuttgart.ims.coref.annotator.ColorProvider;
+import de.unistuttgart.ims.coref.annotator.TypeSystemVersion;
+import de.unistuttgart.ims.coref.annotator.Util;
+import de.unistuttgart.ims.coref.annotator.api.format.Bold;
+import de.unistuttgart.ims.coref.annotator.api.format.Head;
+import de.unistuttgart.ims.coref.annotator.api.format.Italic;
+import de.unistuttgart.ims.coref.annotator.api.v1.Entity;
+import de.unistuttgart.ims.coref.annotator.api.v1.Line;
+import de.unistuttgart.ims.coref.annotator.api.v1.Mention;
+import de.unistuttgart.ims.coref.annotator.api.v1.Segment;
+import de.unistuttgart.ims.uima.io.xml.GenericXmlReader;
+import de.unistuttgart.ims.uima.io.xml.type.XMLElement;
+
+public class TeiReader extends ResourceCollectionReaderBase {
+
+	public static final String PARAM_DOCUMENT_ID = "Document Id";
+
+	@ConfigurationParameter(name = PARAM_DOCUMENT_ID, mandatory = true)
+	String documentId = null;
+
+	@Override
+	public void getNext(CAS aCAS) {
+		ColorProvider colorProvider = new ColorProvider();
+
+		JCas jcas;
+		try {
+			jcas = aCAS.getJCas();
+		} catch (CASException e2) {
+			Annotator.logger.catching(e2);
+			return;
+		}
+
+		MutableMap<String, Entity> entityMap = Maps.mutable.empty();
+
+		GenericXmlReader<DocumentMetaData> gxr = new GenericXmlReader<DocumentMetaData>(DocumentMetaData.class);
+		gxr.setTextRootSelector(null);
+		gxr.setPreserveWhitespace(true);
+
+		// set the document title
+		gxr.addGlobalRule("titleStmt > title", (d, e) -> d.setDocumentTitle(e.text()));
+
+		gxr.addGlobalRule("langUsage[usage=100]", (d, e) -> jcas.setDocumentLanguage(e.attr("ident")));
+
+		gxr.addRule("[ref]", Mention.class, (m, e) -> {
+			String id = e.attr("ref").substring(1);
+			Entity entity = entityMap.get(id);
+			if (entity == null) {
+				entity = new Entity(jcas);
+				entity.addToIndexes();
+				entity.setColor(colorProvider.getNextColor().getRGB());
+				entity.setLabel(m.getCoveredText());
+				entity.setXmlId(id);
+				entityMap.put(id, entity);
+			}
+			m.setEntity(entity);
+		});
+
+		gxr.addRule("head", Head.class);
+		gxr.addRule("emph", Italic.class);
+		gxr.addRule("[rend*=bold]", Bold.class);
+		gxr.addRule("[rend*=italic]", Italic.class);
+		gxr.addRule("lg", Segment.class);
+		gxr.addRule("lb", Line.class, (line, element) -> {
+			String nAttr = element.attr("n");
+
+		});
+		gxr.addRule("l", Line.class,
+				(line, element) -> line.setNumber(element.hasAttr("n") ? Integer.valueOf(element.attr("n")) : -1));
+
+		Resource res = nextFile();
+
+		// Read XMI file
+		try (InputStream is = CompressionUtils.getInputStream(res.getLocation(), res.getInputStream())) {
+			gxr.read(jcas, is);
+		} catch (IOException e1) {
+			Annotator.logger.catching(e1);
+		}
+
+		if (JCasUtil.exists(jcas, DocumentMetaData.class))
+			DocumentMetaData.get(jcas).setDocumentId(documentId);
+		else
+			DocumentMetaData.create(jcas).setDocumentId(documentId);
+
+		Util.getMeta(jcas).setStylePlugin(TeiStylePlugin.class.getName());
+		Util.getMeta(jcas).setTypeSystemVersion(TypeSystemVersion.getCurrent().toString());
+		// TODO: Remove <rs> elements
+		for (XMLElement element : Sets.immutable.withAll(JCasUtil.select(jcas, XMLElement.class))) {
+			if (element.getTag().equalsIgnoreCase("rs"))
+				element.removeFromIndexes();
+		}
+	}
+
+}
---
+++ src/main/java/de/unistuttgart/ims/coref/annotator/plugin/b06/b03/TeiStylePlugin.java
@@ -0,0 +1,61 @@
+package de.unistuttgart.ims.coref.annotator.plugin.b06.b03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
+
+import de.unistuttgart.ims.coref.annotator.StyleManager;
+import de.unistuttgart.ims.coref.annotator.api.format.Bold;
+import de.unistuttgart.ims.coref.annotator.api.format.Head;
+import de.unistuttgart.ims.coref.annotator.api.format.Italic;
+import de.unistuttgart.ims.coref.annotator.plugins.StylePlugin;
+
+public class TeiStylePlugin implements StylePlugin {
+
+	@Override
+	public String getDescription() {
+		return "Style information taken from TEI/XML";
+	}
+
+	@Override
+	public String getName() {
+		return "TEI/XML";
+	}
+
+	@Override
+	public MutableAttributeSet getBaseStyle() {
+		return StyleManager.getDefaultCharacterStyle();
+	}
+
+	@Override
+	public Map<AttributeSet, Type> getSpanStyles(TypeSystem typeSystem, StyleContext context,
+			AttributeSet defaultStyle) {
+		Map<AttributeSet, Type> map = new HashMap<AttributeSet, Type>();
+
+		SimpleAttributeSet sas;
+
+		sas = new SimpleAttributeSet();
+		sas.addAttribute(StyleConstants.Bold, true);
+		map.put(sas, typeSystem.getType(Bold.class.getName()));
+
+		sas = new SimpleAttributeSet();
+		sas.addAttribute(StyleConstants.Italic, true);
+		map.put(sas, typeSystem.getType(Italic.class.getName()));
+
+		sas = new SimpleAttributeSet();
+		sas.addAttribute(StyleConstants.FontSize, (Integer) defaultStyle.getAttribute(StyleConstants.FontSize) + 6);
+		sas.addAttribute(StyleConstants.Bold, true);
+		map.put(sas, typeSystem.getType(Head.class.getCanonicalName()));
+
+		return map;
+	}
+
+}
