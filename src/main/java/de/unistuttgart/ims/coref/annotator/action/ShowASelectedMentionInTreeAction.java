a0118c5e88726c7ab1e0607eb0f2c6cd8838a48a
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2021-03-21 07:22:33 +0100

    new action to select mentions in tree that are touched

---
+++ src/main/java/de/unistuttgart/ims/coref/annotator/action/ShowASelectedMentionInTreeAction.java
@@ -0,0 +1,51 @@
+package de.unistuttgart.ims.coref.annotator.action;
+
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.tree.TreePath;
+
+import org.eclipse.collections.api.set.MutableSet;
+import org.kordamp.ikonli.materialdesign.MaterialDesign;
+
+import de.unistuttgart.ims.coref.annotator.DocumentWindow;
+import de.unistuttgart.ims.coref.annotator.Strings;
+import de.unistuttgart.ims.coref.annotator.api.v1.Mention;
+
+public class ShowASelectedMentionInTreeAction extends DocumentWindowAction implements CaretListener {
+
+	private static final long serialVersionUID = 1L;
+
+	public ShowASelectedMentionInTreeAction(DocumentWindow documentWindow) {
+		super(documentWindow, Strings.ACTION_SHOW_MENTION_IN_TREE, MaterialDesign.MDI_FILE_TREE);
+		putValue(Action.ACCELERATOR_KEY,
+				KeyStroke.getKeyStroke(KeyEvent.VK_T, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
+		setEnabled(false);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Mention toReveal = null;
+		MutableSet<Mention> selectedMentions = getTarget().getTouchedAnnotations(Mention.class);
+		if (selectedMentions.size() == 1) {
+			toReveal = selectedMentions.getOnly();
+		}
+		if (toReveal != null) {
+			Object[] path = getTarget().getDocumentModel().getTreeModel().getPathToRoot(toReveal);
+			TreePath tp = new TreePath(path);
+			this.getTarget().getTree().setSelectionPath(tp);
+			this.getTarget().getTree().scrollPathToVisible(tp);
+		}
+	}
+
+	@Override
+	public void caretUpdate(CaretEvent e) {
+		setEnabled(getTarget().getTouchedAnnotations(Mention.class).size() == 1);
+	}
+
+}
