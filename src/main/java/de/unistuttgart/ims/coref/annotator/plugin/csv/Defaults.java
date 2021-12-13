7bbca7cc5e84eeacd94a769c9c1be132e1d7cd57
bkis <kiss@mailbox.org>
2021-12-13 11:09:49 +0100

    Implement CSV export for ExportMultipleFiles tool
    closes #370

--- src/main/java/de/unistuttgart/ims/coref/annotator/plugin/csv/Defaults.java
+++ src/main/java/de/unistuttgart/ims/coref/annotator/plugin/csv/Defaults.java
@@ -3,6 +3,7 @@ package de.unistuttgart.ims.coref.annotator.plugin.csv;
 import de.unistuttgart.ims.coref.annotator.plugin.csv.CsvExportPlugin.ContextUnit;
 
 public class Defaults {
+	
 	public static final int CFG_OPTION_CONTEXT_WIDTH = 30;
 	public static final ContextUnit CFG_OPTION_CONTEXT_UNIT = ContextUnit.CHARACTER;
 	public static final boolean CFG_OPTION_TRIM = true;
